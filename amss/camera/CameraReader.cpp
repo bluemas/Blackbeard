35e384a52f564156084f648578d418933bc7131c
jungys112 <jungys112@gmail.com>
2017-11-08 23:40:08 -0500

    update CameraReader

--- amss/camera/CameraReader.cpp
+++ amss/camera/CameraReader.cpp
@@ -13,16 +13,28 @@
 
 
 
-
 CameraReader::CameraReader() {
     mCapture =cvCreateCameraCapture(0);   // Open default Camera
     if(!mCapture) {
          printf("Camera Not Initialized\n");
     }
+    if (cvSetCaptureProperty(mCapture, CV_CAP_PROP_FRAME_WIDTH,WIDTH) == 0) { //Set Camera width
+        printf("cvSetCaptureProperty(mCapture, CV_CAP_PROP_FRAME_WIDTH,WIDTH) Failed)\n");
+    }
+
+    if (cvSetCaptureProperty(mCapture, CV_CAP_PROP_FRAME_HEIGHT,HEIGHT) == 0) {// Set camera height
+        printf("cvSetCaptureProperty(mCapture, CV_CAP_PROP_FRAME_HEIGHT,HEIGHT) Failed)\n");
+    }
+
 }
 
 int CameraReader::readCamera(Mat &image) {
+    IplImage * iplCameraImage;
+    iplCameraImage = cvQueryFrame(mCapture); // Get Camera image
+    image = cv::cvarrToMat(iplCameraImage);  // Convert Camera image to Mat format
     
+    if (IsPi3) 
+        flip(image, image, -1);       // if running on PI3 flip(-1)=180 degrees
     return 0;
 }
 

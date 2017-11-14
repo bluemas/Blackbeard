52df980002fb3c34cc68449538dd9e15c6da74cb
Jiseong Kim <gtlovee@gmail.com>
2017-11-13 23:05:55 -0500

    Modify maze data

--- amss/sam/MapData.h
+++ amss/sam/MapData.h
@@ -9,38 +9,47 @@
 #define SAM_MAPDATA_H_
 
 #include <iostream>
+#include <string>
 #include "../common/Constants.h"
+#include "MazeGrid.h"
 
 using namespace std;
 
 class MapData {
 
 public:
-    MapData();
     virtual ~MapData();
+
+    static MapData* getInstance();
 
     void init();
     void setNextDirection(Direction dir);
     Direction getNextDirection();
-    //MazeGrid getCurrMazeGrid();
-    //void setCurrMazeGrid(MazeGrid grid);
-    //void setFrontMazeGrid(MazeGrid grid);
-    void setMazeStatus(char front, char left, char right, char floor);
+    void setWallRecognized(bool forward, bool left, bool right);
+    void setSignRecognized(SignType signType, Direction dir);
+    void setSquareRecognized();
+    void setRedDotRecognized();
     void getMapData();
-    void printMap();
     bool isFirstGrid();
     bool isForwardAvailable();
     bool isLeftAvailable();
     bool isRightAvailable();
     void setBackTrackingMode(bool isBackTracking);
     bool isBackTrackingMode();
+    void printMap();
+    string getMazeString();
 
 private:
     static const int MAZE_SIZE = 20;
+    static const int INIT_POSITION = MAZE_SIZE / 2;
+
+    MapData();
+
+    static MapData *sInstance;
     bool mFirstGrid = true;
-    int mPosX = 10, mPosY = 10;
-    char mMazeArr[MAZE_SIZE][MAZE_SIZE];
-    //MazeGrid mMazeArr[MAZE_SIZE][MAZE_SIZE];
+    int mPosX = INIT_POSITION, mPosY = INIT_POSITION, mMinX = INIT_POSITION,
+        mMinY = INIT_POSITION, mMaxX = INIT_POSITION, mMaxY = INIT_POSITION;
+    MazeGrid mMazeArr[MAZE_SIZE][MAZE_SIZE];
     Direction mRobotDirection = Direction::forward;
     Direction mNextDirection = Direction::forward;
     bool mIsBackTracking = false;
--- amss/sam/MazeGrid.cpp
+++ amss/sam/MazeGrid.cpp
@@ -15,14 +15,91 @@ MazeGrid::~MazeGrid() {
 
 }
 
-void MazeGrid::setRedDotFound() {
+void MazeGrid::init() {
+    mVisited = false;
+    mForwardWall = false;
+    mBackwardWall = false;
+    mLeftWall = false;
+    mRightWall = false;
+    mRedDotFound = false;
+    mSignType = SignType::SignNone;
+    mSignDirection = Direction::none;
+    mEndSqureFound = false;
+}
+
+void MazeGrid::setForwardWall(bool isWall) {
+    mForwardWall = isWall;
+}
+
+void MazeGrid::setBackwardWall(bool isWall) {
+    mBackwardWall = isWall;
+}
+
+void MazeGrid::setLeftWall(bool isWall) {
+    mLeftWall = isWall;
+}
+
+void MazeGrid::setRightWall(bool isWall) {
+    mRightWall = isWall;
+}
+
+bool MazeGrid::isForwardWall() {
+    return mForwardWall;
+}
+
+bool MazeGrid::isBackwardWall() {
+    return mBackwardWall;
+}
+
+bool MazeGrid::isLeftWall() {
+    return mLeftWall;
+}
+
+bool MazeGrid::isRightWall() {
+    return mRightWall;
+}
+
+void MazeGrid::setVisited() {
+
+}
+
+void MazeGrid::setRedDot() {
     mRedDotFound = true;
 }
 
-void MazeGrid::setSignFound(SignType signType) {
+void MazeGrid::setSign(SignType signType, Direction dir) {
     mSignType = signType;
+    mSignDirection = dir;
+}
+
+void MazeGrid::setStartSquare() {
+    mStartSqureFound = true;
 }
 
 void MazeGrid::setEndSquare() {
     mEndSqureFound = true;
+}
+
+bool MazeGrid::isVisited() {
+    return mVisited;
+}
+
+bool MazeGrid::isRedDot() {
+    return mRedDotFound;
+}
+
+bool MazeGrid::isStartSquare() {
+    return mStartSqureFound;
+}
+
+bool MazeGrid::isEndSquare() {
+    return mEndSqureFound;
+}
+
+SignType MazeGrid::getSignType() {
+    return mSignType;
+}
+
+Direction MazeGrid::getSignDirection() {
+    return mSignDirection;
 }
